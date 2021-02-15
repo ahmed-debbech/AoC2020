4de24d23d993339f03611f687a630d23338572f2
ahmed-debbech <debbech.ahmed@gmail.com>
2021-02-15 19:25:08 +0100

    write part 2 day15 it works very slooooows

--- day15/puzzle2.c
+++ day15/puzzle2.c
@@ -2,9 +2,14 @@
 #include <stdlib.h>
 #include <string.h>
 
-int * get_data(int * m){
+typedef struct{
+    long num;
+    long last_seen;
+}entry;
+
+long * get_data(long * m, entry ** uni, long *size1){
     FILE * f = fopen("input.txt", "r");
-    int * arr = malloc(sizeof(int) * ((*m)+1));
+    long * arr = malloc(sizeof(long) * ((*m)+1));
     if(f != NULL){
         char str[100];
         int i=0;
@@ -17,43 +22,74 @@ int * get_data(int * m){
                 arr[*m] = atoi(buf);
                 strcpy(buf, "");
                 (*m)++;
-                arr = realloc(arr, sizeof(int) * ((*m)+1));
+                arr = realloc(arr, sizeof(long) * ((*m)+1));
             }else{
                 strncat(buf, &str[i], 1);
             }
+        }
+        for(i=0; i<=(*m)-1; i++){
+            (*uni)[*size1].num = arr[i];
+            (*uni)[*size1].last_seen = i;
+            (*size1)++;
+            *uni = realloc(*uni, sizeof(entry) * ((*size1)+1));
         }
         fclose(f);
     }
     return arr;
 }
-int guess_number(int * arr, int size){
-    //for(int h=0; h<=size-1; h++){printf("arr: %d\n", arr[h]);}
-    int index = size-1;
-    int number = 0;
-    int i;
-    printf("size: %d\n", size);
-    for(i=size-2; i>=0; i--){
-        if(arr[i] == arr[index]){
-            number = index - i;
-            return number;
+long say_age_or_0(long index, long number_look, entry uni[], long size){
+    for(long k =0; k<=size-1; k++){
+        if(number_look == uni[k].num){
+            if((index - uni[k].last_seen) == 0){
+                return 0;
+            }else{
+                return index - uni[k].last_seen;
+            }
         }
+    }
+    return -1; //it is a new number should be added to UNI
+}
+int guess_number(long * arr, long size, entry ** uni, long * size1){
+    long index = size;
+    long h = say_age_or_0(index-1, arr[index-1] ,*uni, *size1);
+    if(h == -1){
+        (*size1)++;
+        *uni = realloc(*uni, sizeof(entry) * (*size1));
+        (*uni)[(*size1)-1].num = arr[index-1];
+        (*uni)[(*size1-1)].last_seen = index-1;
+        return 0;
+    }
+    if(h == 0){
+        return 0;
+    }else{
+        for(int i=0; i<=((*size1)-1); i++){
+            if(arr[index-1] == (*uni)[i].num){
+                (*uni)[i].last_seen = index-1;
+            }
+        }
+        return h;
     }
     return 0;
 }
 int main(){
-    int size = 0;
-    int * arr = NULL;
-    arr = get_data(&size);
-    for(int h=0; h<=size-1; h++) printf("arr: %d\n", arr[h]);
-    printf("sii : %d\n", size);
-    for(int i=0; i<=30000000; i++){
-        printf("size: %d\n", size);
-        int num = guess_number(arr, size);
+    //line 24 go see
+    long size = 0;
+    long * arr = NULL;
+    long size_sec=0;
+    entry * unique = malloc(sizeof(entry)*1);
+    arr = get_data(&size, &unique, &size_sec);
+    //for(long h=0; h<=10; h++){printf("@count (%ld) = arr: %ld + occ: %ld\n", h+1, unique[h].num, unique[h].last_seen);}
+
+    for(long i=size; i<=30000000; i++){
+        printf("counter: %ld\n", i);
+        long num = guess_number(arr, size, &unique, &size_sec);
         size++;
-        arr = realloc(arr, sizeof(int) * size);
+        arr = realloc(arr, sizeof(long) * size);
         arr[size-1] = num;
     }
-    for(int h=0; h<=30000000; h++){printf("count (%d) = arr: %d\n", h+1, arr[h]);}
-    printf("THE RESULT IS: %d\n", arr[2019]);
+    for(long h=0; h<=size_sec-1; h++){printf("count (%ld) = arr: %ld + occ: %ld\n", h+1, unique[h].num, unique[h].last_seen);}
+    printf("--------\n");
+    for(long h=0; h<=size-1; h++){printf("count (%ld) = arr: %ld\n", h+1, arr[h]);}
+    printf("THE RESULT IS: %ld\n", arr[2019]);
     return 0;
 }
